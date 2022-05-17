a3c88d8b1af023496c841cbf50cd3a027330fcc5
kkarrkid7 <kkarrkid7@gmail.com>
2022-05-17 16:10:47 +0900

    [Update] Apidoc Added

--- src/routes/root/root.ctrl.js
+++ src/routes/root/root.ctrl.js
@@ -1,4 +1,39 @@
 "use strict"
+
+/**
+ * 로그인
+ * 
+ * @api {post} /login 로그인
+ * 
+ * @apiName login
+ * @apiGroup Auth
+ * @apiVersion 1.0.0
+ * @apiDescription 로그인 쿼리를 요청합니다.
+ * 
+ * @apiParam {String} Users id, password
+ * 
+ * @apiParamExample {json} client id, pw value
+ * {
+ *  "id": "qwer",
+ *  "password": "123456"
+ * }
+ * 
+ * @apiSuccess {String} function return true
+ * 
+ * @apiSuccessExample {json} Response (example):
+ * {
+ *  "success": true
+ * }
+ * @apiError error function return false and message
+ * 
+ * @apiErrorExample {json} Error (example):
+ * {
+ *  success: false,
+ *  msg: "출력되는 에러 메시지"
+ * }
+ * 
+ */
+
 const UserStorage = require("../../models/UserStorage")
 const User = require("../../models/User")
 
